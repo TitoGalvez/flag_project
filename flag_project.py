f3bd7f3c7053874ff6e39ab1bf066b38d07e6237
Tito G <105616821+TitoGalvez@users.noreply.github.com>
2023-01-12 18:22:06 -0800

    Add files via upload

---
+++ flag_project.py
@@ -0,0 +1,11 @@
+first_counter = 0
+
+while first_counter < 5:
+    print("**********-------")
+    first_counter += 1
+    
+second_counter = 0
+
+while second_counter < 4:
+    print("-----------------")
+    second_counter += 1
